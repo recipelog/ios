c800d76c224f0da5080700ca5b044354359e5b5f
NAKAGAKI Kenji <nakaken0629@gmail.com>
2021-04-28 19:25:28 +0900

    init project

---
+++ recipelog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipelog
+//
+//  Created by NAKAGAKI Kenji on 4/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recipelog/recipelogApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipelogApp.swift
+//  recipelog
+//
+//  Created by NAKAGAKI Kenji on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct recipelogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
